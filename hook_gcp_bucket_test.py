d38f26641066017286ff8a12a2a057402b1a186e
dhruvkumar <dhruvpatel719@yahoo.in>
2022-01-19 19:57:34 -0400

    buf solve processs_manifest_json_file

--- hook_gcp_bucket_test.py
+++ hook_gcp_bucket_test.py
@@ -73,7 +73,7 @@ def process_manifest_json_file(**kwargs):
             data = json.loads(data)
             # print(data['filepath'])
             path = data['path'][1:]
-            avro_files_list.extend([ path + filepath['name'] for filepath in data['files']])
+            avro_files_list.extend([ path + "/" +  filepath['name'] for filepath in data['files']])
         except exceptions.NotFound as e:
             logger = logging.getLogger("airflow.task")
             logger.error("Exception Not found:" + str(e))
