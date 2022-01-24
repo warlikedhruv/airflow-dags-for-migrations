3093b5536bae3f446699681b828ada8a37641d39
dhruvkumar <dhruvpatel719@yahoo.in>
2022-01-24 18:32:15 -0400

    http post

---
+++ http_post_req.py
@@ -0,0 +1,9 @@
+import json
+
+resp = {}
+def function_post():
+    api_response = json.loads(api_response)
+    url = "http://google.com/data"
+    data = {'keyword': keyword}
+    print(data)
+    #r = requests.post(url,  headers={'Authorization': 'access_token myToken'}, data=data,)
