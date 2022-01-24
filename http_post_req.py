e6babecf4e9e741860b9d7cfe43835c36be295fd
dhruvkumar <dhruvpatel719@yahoo.in>
2022-01-24 19:42:38 -0400

    http post

--- http_post_req.py
+++ http_post_req.py
@@ -10,3 +10,15 @@ def function_post():
     headers = {'Authorization': "Bearer {}".format(token)}
     r = requests.post(url,  headers=headers, data=data)
     print(r.text)
+    r = json.loads(r.text)
+    report_id = r['']['']
+    return report_id
+
+def download_data():
+    report_id = "rows"
+    url= "anyurl" + report_id
+    response = requests.post(url)
+    data = response.content
+    print(data)
+
+
