4f41af7c31800928e438d837999ad5e7230efec9
dhruvkumar <dhruvpatel719@yahoo.in>
2022-01-25 19:39:23 -0400

    loda_bq

--- http_post_req.py
+++ http_post_req.py
@@ -1,6 +1,7 @@
 import json
 import requests
-
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
+from datetime import datetime
 resp = {}
 
 
@@ -19,7 +20,7 @@ def function_post():
 
 def download_data(**kwargs):
     x_Comm_var = kwargs['ti']
-    response = x_Comm_var.xcomm_pull(task_id='function_post')
+    response = x_Comm_var.xcomm_pull(task_ids='function_post')
     report_id = response['report_id']  # new
     token = response['token']  # new
     url = "https://google.com" + report_id
@@ -39,9 +40,30 @@ def download_data(**kwargs):
 
 def load_bq(**kwargs):
     x_Comm_var = kwargs['ti']
-    data = x_Comm_var.xcomm_pull(task_id='download_data')
+    data = x_Comm_var.xcomm_pull(task_ids='download_data')
     data_filter = list(filter(bool, data.splitlines()))
     columns = data_filter[0].split(",")
     values = data_filter[1].split(",")
-    print(columns)
-    print(values)
+    final_data = {}
+    for key, value in zip(columns, values):
+        final_data[key] = value
+    sql_Statement = "INSERT INTO {schemaName} " \
+                    "(database_name, table_name, transaction_dt, apn_impression_count, load_ts )" \
+                    "select {database_name}, {table_name}, {transaction_dt}, {apn_impression_count}, {load_ts}"
+    sql_Statement.format(database_name="",table_name="",
+                         transaction_dt=final_data['day'],
+                         apn_impression_count =final_data['imps']
+                         , load_ts=datetime.now())
+    bq_operator = BigQueryHook(bigquery_conn_id=bQ_CONN_ID,use_legacy_sql=False )
+    service = bq_operator.get_service()
+    run_query_operator = BigQueryBaseCursor(service=service, project_id="")
+    run_query_operator.run_query(
+        sql="",
+        destination_dataset_table="",
+        allow_large_results=True,
+        use_legacy_sql=False
+        write_disposition='WRITE_APPEND',
+
+    )
+
+
