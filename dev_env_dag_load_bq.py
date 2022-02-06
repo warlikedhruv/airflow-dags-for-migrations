4de8001fe365d275d80a1c4aa6e2b59ea4b181b8
dhruvkumar <dhruvpatel719@yahoo.in>
2022-02-06 00:40:31 -0400

    Load BQ dag dev

--- dev_env_dag_load_bq.py
+++ dev_env_dag_load_bq.py
@@ -3,15 +3,16 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 import os
+from airflow.utils.email import send_email_smtp
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/opt/airflow/dags/mineral-order-337219-e4d095ffa62f.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/opt/airflow/dags/mineral-order-337219-e4d095ffa62f.json"  # do not use
 
 """
 ENV VARIABLES
 """
 BUCKET_NAME = "airflow-test-bucket-1107"
 storage_client = storage.Client()
-
+today_dt = datetime.now().date()
 """
 
 """
@@ -20,51 +21,34 @@ STEP:1
 """
 
 
-def scan_bucket_for_manifest_files():
+def scan_bucket():
+    import re
     prefix = "manifest/"
     blobs = storage_client.list_blobs(BUCKET_NAME, prefix=prefix, delimiter="/")
-    manifest_json_files_list = [str(blob.name) for blob in blobs if
-                                len(list(filter(None, str(blob.name).split("/")))) == 2]
+    yesterday_dt = today_dt - timedelta(2)
+    kv_feed_regex = prefix + "auction_kv_labels_feed-11303-" + yesterday_dt.strftime("%Y%m%d") + "\w+"
+    standard_feed_regex = prefix + "standard_feed_feed-11303-" + yesterday_dt.strftime("%Y%m%d") + "\w+"
+    kv_feed = []
+    standard_feed = []
+    for blob in blobs:
+        if re.match(kv_feed_regex, blob.name):
+            kv_feed.append(blob.name)
+        elif re.match(standard_feed_regex, blob.name):
+            standard_feed.append(blob.name)
 
-    return {"manifest_json_files_list": manifest_json_files_list}
+    return {"kv_feed": kv_feed, "standard_feed": standard_feed}
 
-
-def get_yesterday_files(**kwargs):
-    manifest_files = scan_bucket_for_manifest_files()['manifest_json_files_list']
-
-    # getting yesterday's date
-    today_dt = datetime.now().date()
-    yesterday_dt = today_dt - timedelta(1)
-
-    # saving yesterday and old files paths
-    yesterday_json_files = []
-    old_json_files = []
-
-    for file_path in manifest_files:
-        try:
-            file_dt = datetime.strptime(file_path.split("-")[2], "%Y%m%d%H").date()
-            print(file_dt)
-            if yesterday_dt == file_dt:
-                yesterday_json_files.append(file_path)
-            elif file_dt < yesterday_dt:
-                print("old file", file_path)
-                old_json_files.append(file_path)
-        except Exception as e:
-            print(str(e))
-
-    return {"yesterday_json_files": yesterday_json_files, "old_json_files": old_json_files}
 
 """
-Step-2
+STEP:2
 """
-def duplicate_hour_check(**kwargs):
-    xComm_var = kwargs['ti']
-    yesterday_json_files = xComm_var.xcom_pull(task_ids="get_yesterday_files")['yesterday_json_files']
 
+
+def filter_duplicate_hours(feed_path):
     filter_hours_dict = {}
     duplicate_file_list = []
 
-    for filepath in yesterday_json_files:
+    for filepath in feed_path:
         try:
             file_split = filepath.split("-")
             key = file_split[2]
@@ -88,19 +72,79 @@ def duplicate_hour_check(**kwargs):
     return {"processed_files": processed_files, "duplicate_file_list": duplicate_file_list}
 
 
+def remove_duplicate_hours(**kwargs):
+    xComm_var = kwargs['ti']
+    feed_files = xComm_var.xcom_pull(task_ids='scan_bucket')
+
+    filtered_kv_feeds = filter_duplicate_hours(feed_files['kv_feed'])
+
+    filtered_standard_feeds = filter_duplicate_hours(feed_files['standard_feed'])
+
+    return {"kv_feed": filtered_kv_feeds, "standard_feed": filtered_standard_feeds}
+
+
 """
 STEP - 3
 """
+
+
+def prepared_data_manifest_stg(feed_type):
+    valid_files = feed_type["processed_files"]
+    duplicate_files = feed_type["duplicate_file_list"]
+    values = []
+
+    for file_path in valid_files:
+        manifest_name = file_path.split("/", 1)[-1]
+        transaction_dt = datetime.strptime(file_path.split("-")[2], "%Y%m%d%H").date()
+        transaction_hr = datetime.strptime(file_path.split("-")[2], "%Y%m%d%H").hour
+        file_valid_check = "N"
+        load_dt = datetime.strptime(file_path.split("-")[2], "%Y%m%d%H").date() + timedelta(1)
+        row = f"('{manifest_name}', '{transaction_dt}', '{transaction_hr}', '{file_valid_check}', '{load_dt}')"
+        values.append(row)
+
+    for file_path in duplicate_files:
+        manifest_name = file_path.split("/", 1)[-1]
+        transaction_dt = datetime.strptime(file_path.split("-")[2], "%Y%m%d%H").date()
+        transaction_hr = datetime.strptime(file_path.split("-")[2], "%Y%m%d%H").hour
+        file_valid_check = "Y"
+        load_dt = datetime.strptime(file_path.split("-")[2], "%Y%m%d%H").date() + timedelta(1)
+        row = f"('{manifest_name}', '{transaction_dt}', '{transaction_hr}', '{file_valid_check}', '{load_dt}')"
+        values.append(row)
+
+    return values
+
+
 def load_manifest_table(**kwargs):
     from google.cloud import bigquery
-    insert_client = bigquery.Client()
-    table_id_status = ""
-    job_config = bigquery.QueryJobConfig(labels= {})
-    insert_job = insert_client.query("INSERT INTO `mineral-order-337219.test_dags.dummy_table` VALUES ('1', '2')", location="US", job_config=job_config)
-    insert_job.result()
+
+    xComm_var = kwargs['ti']
+    feed_files = xComm_var.xcom_pull(task_ids='remove_duplicate_hours')
+
+    insert_job_kv_feeds_values = prepared_data_manifest_stg(feed_files['kv_feed'])
+    insert_kv_feeds_sql = "INSERT INTO `mineral-order-337219.test_dags.dummy_table` VALUES " + ",".join(
+        insert_job_kv_feeds_values)
+    print(insert_kv_feeds_sql)
+
+    insert_job_kv_feeds_values = prepared_data_manifest_stg(feed_files['standard_feed'])
+    insert_kv_feeds_sql = "INSERT INTO `mineral-order-337219.test_dags.dummy_table` VALUES " + ",".join(
+        insert_job_kv_feeds_values)
+    print(insert_kv_feeds_sql)
+
+    # insert_client = bigquery.Client()
+    # table_id_status = ""
+    # job_config = bigquery.QueryJobConfig(labels={})
+    # insert_job = insert_client.query("INSERT INTO `mineral-order-337219.test_dags.dummy_table` VALUES ('1', '2')",
+    #                                  location="US", job_config=job_config)
+    # insert_job.result()
 
 
+"""
+STEP-4
+"""
 
+
+def send_warning_email(**kwargs):
+    send_email_smtp("test@email.com", "TEST", "BODY MESSAGE")
 
 
 default_args = {
@@ -116,24 +160,22 @@ default_args = {
 
 dag = DAG("hook_gcp_bucket_final_dev", default_args=default_args, schedule_interval=None)
 
-read_bucket_files_t1_1 = PythonOperator(
-    task_id='get_yesterday_files',
-    python_callable=get_yesterday_files,
+scan_bucket_t1_1 = PythonOperator(
+    task_id='scan_bucket',
+    python_callable=scan_bucket,
     provide_context=True,
     dag=dag)
 
 duplicate_hour_check_t2_1 = PythonOperator(
-    task_id='duplicate_hour_check',
-    python_callable=duplicate_hour_check,
+    task_id='remove_duplicate_hours',
+    python_callable=remove_duplicate_hours,
     provide_context=True,
     dag=dag)
-
+#
 load_manifest_table_t3_1 = PythonOperator(
     task_id='load_manifest_table',
     python_callable=load_manifest_table,
     provide_context=True,
     dag=dag)
 
-
-
-read_bucket_files_t1_1 >> duplicate_hour_check_t2_1 >> load_manifest_table_t3_1
+scan_bucket_t1_1 >> duplicate_hour_check_t2_1 >> load_manifest_table_t3_1
