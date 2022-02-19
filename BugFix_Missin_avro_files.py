c380a71c3335f6d807e4ef6a5c640245b5a53f76
dhruvkumar <dhruvpatel719@yahoo.in>
2022-02-19 12:10:11 -0400

    run_query_function

--- BugFix_Missin_avro_files.py
+++ BugFix_Missin_avro_files.py
@@ -1,33 +1,55 @@
-def is_path_exists(bucket_name: str, files_list: list):
-    missing_files = []
-    for path in files_list:
-        bucket = storage_client.get_bucket(bucket_name)
-        blob = bucket.blob(path)
-        if not blob.exists():
-            missing_files.append(path)
-    if missing_files:
-        missing_avro_files_failure_email(...)  # same parameters
-    return None
-
+# def is_path_exists(bucket_name: str, files_list: list):
+#     missing_files = []
+#     for path in files_list:
+#         bucket = storage_client.get_bucket(bucket_name)
+#         blob = bucket.blob(path)
+#         if not blob.exists():
+#             missing_files.append(path)
+#     if missing_files:
+#         missing_avro_files_failure_email(...)  # same parameters
+#     return None
+#
 
 """
 Import Ymal Files and environment
 """
 import os
 
-ENVIRONMENT = os.getenv("env").upper()
-PARAMS_PATH = "/home/airflow/gcs/dags"
+# ENVIRONMENT = os.getenv("env").upper()
+# PARAMS_PATH = "/home/airflow/gcs/dags"
 
 
-def read_parameters_from_file(filename: str, env: str):
-    import yaml
-    with open(filename, 'r') as file:
-        params = yaml.full_load(file)
-        my_config_dict = params[env]
-        return my_config_dict
+# def read_parameters_from_file(filename: str, env: str):
+#     import yaml
+#     with open(filename, 'r') as file:
+#         params = yaml.full_load(file)
+#         my_config_dict = params[env]
+#         return my_config_dict
+#
+#
+# config_values = read_parameters_from_file(PARAMS_PATH + "/config.yml", ENVIRONMENT)
+# PIPELINE_PROJECT_ID = config_values['pipeline_project_id']
+# EMAIL = config_values['email']
+# SERVICE_ACC = config_values['service_account']
 
 
-config_values = read_parameters_from_file(PARAMS_PATH + "/config.yml", ENVIRONMENT)
-PIPELINE_PROJECT_ID = config_values['pipeline_project_id']
-EMAIL = config_values['email']
-SERVICE_ACC = config_values['service_account']
+
+def run_query_function(**kwargs):
+    from google.cloud import bigquery
+    client = bigquery.Client() # or impersonate client
+    query_job = client.query(
+        """
+        SELECT
+          CONCAT(
+            'https://stackoverflow.com/questions/',
+            CAST(id as STRING)) as url,
+          view_count
+        FROM `bigquery-public-data.stackoverflow.posts_questions`
+        WHERE tags like '%google-bigquery%'
+        ORDER BY view_count DESC
+        LIMIT 10"""
+    )
+
+    results = query_job.result()
+    for row in results:
+        print("{} : {} views".format(row.url, row.view_count))
