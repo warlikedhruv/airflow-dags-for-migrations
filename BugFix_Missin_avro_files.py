56e79ff85c4e96a18a1712d5867842d9814211e8
dhruvkumar <dhruvpatel719@yahoo.in>
2022-02-19 11:58:14 -0400

    YAML dev

--- BugFix_Missin_avro_files.py
+++ BugFix_Missin_avro_files.py
@@ -6,6 +6,28 @@ def is_path_exists(bucket_name: str, files_list: list):
         if not blob.exists():
             missing_files.append(path)
     if missing_files:
-        missing_avro_files_failure_email(...) # same parameters
+        missing_avro_files_failure_email(...)  # same parameters
     return None
 
+
+"""
+Import Ymal Files and environment
+"""
+import os
+
+ENVIRONMENT = os.getenv("env").upper()
+PARAMS_PATH = "/home/airflow/gcs/dags"
+
+
+def read_parameters_from_file(filename: str, env: str):
+    import yaml
+    with open(filename, 'r') as file:
+        params = yaml.full_load(file)
+        my_config_dict = params[env]
+        return my_config_dict
+
+
+config_values = read_parameters_from_file(PARAMS_PATH + "/config.yml", ENVIRONMENT)
+PIPELINE_PROJECT_ID = config_values['pipeline_project_id']
+EMAIL = config_values['email']
+SERVICE_ACC = config_values['service_account']
