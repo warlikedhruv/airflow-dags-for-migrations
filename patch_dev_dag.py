8a398d3813863b933b6c5d1fa1f9b099b1475f93
dhruvkumar <dhruvpatel719@yahoo.in>
2022-03-22 19:12:15 -0300

    patch fix dev

--- patch_dev_dag.py
+++ patch_dev_dag.py
@@ -7,7 +7,7 @@ def get_execution_date(execution_dt):
     task_instance = get_task_instance.get_task_instance("dag_name", "start",
                                                         execution_dt)  # change the 'dag_name' with the current dag name and "start" will be Dummy operatopr = Start
     execution_start_date = task_instance.start_date
-    return execution_start_date.date() - timedelta(1)
+    return execution_start_date.date()
 
 
 def transaction_report_email(**kwargs):
