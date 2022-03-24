8185fb389a7d4457e9e0687ca3dd8a25ae03e794
dhruvkumar <dhruvpatel719@yahoo.in>
2022-03-24 19:31:46 -0300

    Patch api post

--- patch_dev_dag.py
+++ patch_dev_dag.py
@@ -1,22 +1,33 @@
-from datetime import timedelta
+# from datetime import timedelta
+#
+# from airflow.api.common.experimental import get_task_instance
+#
+#
+# def get_execution_date(execution_dt):
+#     task_instance = get_task_instance.get_task_instance("dag_name", "start",
+#                                                         execution_dt)  # change the 'dag_name' with the current dag name and "start" will be Dummy operatopr = Start
+#     execution_start_date = task_instance.start_date
+#     return execution_start_date.date()
+#
+#
+# def transaction_report_email(**kwargs):
+#     transaction_dt = get_task_instance(kwargs['execution_date'] - timedelta(0))
+#     """
+#     Rest will be the same
+#     """
+#     if my_config_values['trans_plus_one'] == 'None':
+#         transaction_dt = kwargs['execution_date'].date() - timedelta(1)
+#     else:
+#         ...
+#
+import json
 
-from airflow.api.common.experimental import get_task_instance
+
+def api_post():
 
 
-def get_execution_date(execution_dt):
-    task_instance = get_task_instance.get_task_instance("dag_name", "start",
-                                                        execution_dt)  # change the 'dag_name' with the current dag name and "start" will be Dummy operatopr = Start
-    execution_start_date = task_instance.start_date
-    return execution_start_date.date()
-
-
-def transaction_report_email(**kwargs):
-    transaction_dt = get_task_instance(kwargs['execution_date'] - timedelta(0))
-    """
-    Rest will be the same 
-    """
-    if my_config_values['trans_plus_one'] == 'None':
-        transaction_dt = kwargs['execution_date'].date() - timedelta(1)
+    r = json.loads(r.text)
+    if 'report_id' in r['response'].keys():
+        same code...
     else:
-        ...
-
+        token, report_id = None, None
