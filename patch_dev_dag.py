1b57d9ba0910fd15e22f48257a8d67a0d19f9712
dhruvkumar <dhruvpatel719@yahoo.in>
2022-03-25 19:35:04 -0300

    patch 2

--- patch_dev_dag.py
+++ patch_dev_dag.py
@@ -1,27 +1,72 @@
-# from datetime import timedelta
-#
-# from airflow.api.common.experimental import get_task_instance
-#
-#
-# def get_execution_date(execution_dt):
-#     task_instance = get_task_instance.get_task_instance("dag_name", "start",
-#                                                         execution_dt)  # change the 'dag_name' with the current dag name and "start" will be Dummy operatopr = Start
-#     execution_start_date = task_instance.start_date
-#     return execution_start_date.date()
-#
-#
-# def transaction_report_email(**kwargs):
-#     transaction_dt = get_task_instance(kwargs['execution_date'] - timedelta(0))
-#     """
-#     Rest will be the same
-#     """
-#     if my_config_values['trans_plus_one'] == 'None':
-#         transaction_dt = kwargs['execution_date'].date() - timedelta(1)
-#     else:
-#         ...
-#
-import json
+import time
+from datetime import datetime, timedelta, timezone
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from google.cloud import storage
+from airflow.operators.dummy import DummyOperator
+import os
+from airflow.api.common.experimental import get_task_instance
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/opt/airflow/dags/mineral-order-337219-e4d095ffa62f.json"  # do not use
+
+# global variables
+start_date = datetime(2022, 2, 25)
+BUCKET_NAME = "airflow-test-bucket-1107"
+DESTINATION_BUCKET_NAME = "airflow-test-bucket-1107"
+storage_client = storage.Client()
+today_dt = datetime.now().date()
+manifest_history_file_list = []
+
+load_ts: datetime
+
+start_date = datetime(2022, 2, 25)
+default_args = {
+    "owner": "dhruv-airflow-test-dev",
+    "depends_on_past": False,
+    "start_date": start_date,
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG("dev_dag_patch", default_args=default_args,
+          schedule_interval='25 22 * * *',
+          dagrun_timeout=timedelta(seconds=2),
+          catchup=False)
+
+global_load_ts = '{{ yesterday_ds_nodash }}'
+
+def test_1(**kwargs):
+    print(global_load_ts)
+    return True
+
+
+def get_last_exec_date(dag_id='dev_dag_patch'):
+    print(dag.get_last_dagrun(include_externally_triggered=True))
+    return True
 
 
 
-        
+
+python_sleep_1 = PythonOperator(
+    task_id='python_sleep',
+    python_callable=test_1,
+    dag=dag)
+
+test_2 = PythonOperator(
+    task_id='test_2',
+    python_callable=get_last_exec_date,
+    dag=dag
+)
+
+
+
+# archive_duplicate_files_t4_2 = PythonOperator(
+#     task_id='get_load_and_today_dt_2',
+#     python_callable=get_load_and_today_dt,
+#     provide_context=True,
+#     dag=dag)
+
+
