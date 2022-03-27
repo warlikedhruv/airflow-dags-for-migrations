2b44e86fbda1e3ad30809dab73a5afeb33850a58
dhruvkumar <dhruvpatel719@yahoo.in>
2022-03-27 15:08:45 -0300

    change 2

--- patch_dev_dag.py
+++ patch_dev_dag.py
@@ -6,6 +6,7 @@ from google.cloud import storage
 from airflow.operators.dummy import DummyOperator
 import os
 from airflow.api.common.experimental import get_task_instance
+from airflow.models.dagrun import DagRun
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/opt/airflow/dags/mineral-order-337219-e4d095ffa62f.json"  # do not use
 
@@ -32,11 +33,12 @@ default_args = {
 }
 
 dag = DAG("dev_dag_patch", default_args=default_args,
-          schedule_interval='25 22 * * *',
+          schedule_interval='07 18 * * *',
           dagrun_timeout=timedelta(seconds=2),
           catchup=False)
 
-global_load_ts = '{{ yesterday_ds_nodash }}'
+global_load_ts = "{{{ yesterday_ds_nodash }}}"
+
 
 def test_1(**kwargs):
     print(global_load_ts)
@@ -44,11 +46,14 @@ def test_1(**kwargs):
 
 
 def get_last_exec_date(dag_id='dev_dag_patch', **kwargs):
+    template_fields = ('dag_run')
+    print("Execution", kwargs['execution_date'])
+    print("ACUTAL TIME WITHOUT TEMPLATE:", kwargs['dag_run'].start_date)
+    # print("WITH TEMPLATE", kwargs['dag_run']['start_date'])
     print(dag.get_last_dagrun(include_externally_triggered=True))
     print("ACTUAL TIME", kwargs['key1'])
+    print("DAG RUN", dag.start_date)
     return True
-
-
 
 
 python_sleep_1 = PythonOperator(
@@ -64,12 +69,8 @@ test_2 = PythonOperator(
     dag=dag
 )
 
-
-
 # archive_duplicate_files_t4_2 = PythonOperator(
 #     task_id='get_load_and_today_dt_2',
 #     python_callable=get_load_and_today_dt,
 #     provide_context=True,
 #     dag=dag)
-
-
