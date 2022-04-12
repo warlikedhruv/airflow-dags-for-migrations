a17fa8faa0324eb18be7939182b8509dde4b40ca
dhruvkumar <dhruvpatel719@yahoo.in>
2022-04-12 19:38:54 -0300

    v2 dag

--- V2-prod-dag.py
+++ V2-prod-dag.py
@@ -38,12 +38,27 @@ dag = DAG("prod-dag-v2", default_args=default_args,
           catchup=False)
 
 """
-INSERT INTO log_manifest_stg () VALUES 1,2,3
+GENERAL FUNCTIONS
 """
+def insert_row_into_table(bq_client, tablename:str, rows:list):
+    # insert into the bigquery table
+    errors = bq_client.insert_rows_json(tablename, rows)  # Make an API request.
+    if errors == []:
+        print("New rows have been added to {table}".format(table=tablename))
+    else:
+        print("Encountered errors while inserting rows: {}".format(errors))
+
+def change_control_table(bq_client, transaction_dt, processed_flag):
+    dml_statement = (
+        "UPDATE `mineral-order-337219.test_dags.control_table` \
+        SET processed_flag = {processed_flag} \
+        WHERE transaction_dt = '{transaction_dt}'".format(transaction_dt=transaction_dt,processed_flag=processed_flag))
+
+    query_job = bq_client.query(dml_statement)  # API request
+    query_job.result()  # Waits for statement to finish
 
 
 def scan_and_load_file_to_table():
-    insert_value_fmt = "({filepath}, {transaction_dt}),"
     rows_to_insert = []
     prefix = "manifest/"
     blobs = storage_client.list_blobs(BUCKET_NAME, prefix=prefix)
@@ -63,21 +78,24 @@ def load_manifest_stagging_table():
     client = bigquery.Client()
 
     rows = scan_and_load_file_to_table()
-    print(rows)
+    insert_row_into_table(client, "mineral-order-337219.test_dags.log_manifest_stg", rows)
 
-    errors = client.insert_rows_json("mineral-order-337219.test_dags.log_manifest_stg", rows)  # Make an API request.
-    if errors == []:
-        print("New rows have been added.")
-    else:
-        print("Encountered errors while inserting rows: {}".format(errors))
 
-def detect_history_transaction():
+def is_history_date(bq_client, transaction_dt):
+    query = "SELECT transaction_dt, processed_flag from `mineral-order-337219.test_dags.control_table` " \
+            "WHERE transaction_dt = '{transaction_dt}'".format(transaction_dt=transaction_dt)
+    query_job = bq_client.query(query)
+    results = query_job.result()
+    if results.total_rows != 0:
+        print("date Exists")
+        return True
+    return False
 
 
 def load_control_table():
     from google.cloud import bigquery
     client = bigquery.Client()
-
+    rows_insert_for_control_table = []
     query = "SELECT count(filename) as count, transaction_dt \
             FROM `mineral-order-337219.test_dags.log_manifest_stg` \
             GROUP BY transaction_dt"
@@ -85,8 +103,15 @@ def load_control_table():
     query_job = client.query(query)
     results = query_job.result()
     for result in results:
-        print(result.count, result.transaction_dt)
+        if result.count >= 48:
+            rows_insert_for_control_table.append({"transaction_dt": result.transaction_dt})
+        elif is_history_date(client, result.transaction_dt):
+            # update the processed flag to false to again process the history files
+            change_control_table(client, result.transaction_dt, 'false')
 
+
+    # insert new transaction dates into control table
+    insert_row_into_table(client, rows_insert_for_control_table)
 
 start = DummyOperator(
     task_id="start",
@@ -94,7 +119,15 @@ start = DummyOperator(
     dag=dag
 )
 
-task1 = PythonOperator(
+# scan the manifest files and insert into manifest stagging table
+task_1 = PythonOperator(
+    task_id='load_manifest_stagging_table',
+    python_callable=load_manifest_stagging_table,
+    dag=dag
+)
+
+# detect the new transaction_dt and history_dt and ignore future dates
+task_2 = PythonOperator(
     task_id='load_control_table',
     python_callable=load_control_table,
     dag=dag)
@@ -104,3 +137,5 @@ end = DummyOperator(
     trigger_rule="all_success",
     dag=dag
 )
+
+start >> task_1 >> task_2 >> end
